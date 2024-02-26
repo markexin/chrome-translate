60b02c3547064a78ad3238a44c1f98930eab3e90
maqun1 <markexin@gmail.com>
2024-02-26 17:29:43 +0800

    feat: 完成插件基本的划词功能

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # gpt-translate
+
+<img src="./assets/icon-128.png" width="100" height="100" />
+
 基于 OpenAI 定制化chrome插件，保证流畅的翻译效果 
-
-
--- src/pages/content/main.js
+++ src/pages/content/main.js
@@ -33,31 +33,48 @@ function offSet (curEle) {
 function findExit () {
   const domList = document.getElementsByClassName('gpt-translate')
   if (domList.length >= 1) {
-    domList[0].removeChild()
+    while (domList[0].hasChildNodes()) {
+      domList[0].removeChild(domList[0].firstChild)
+    }
     return true
   }
   return false
 }
 
+let right = 0
+
 function appIncon () {
-  const focusNode = document.getSelection().focusNode.parentElement
-  const body = document.body
-  let parentDiv
-  if (findExit()) {
-    parentDiv = document.getElementsByClassName('gpt-translate')[0]
-  } else {
-    parentDiv = document.createElement('div')
+  try {
+    const focusNode = document.getSelection().focusNode.parentElement
+    const body = document.body
+    let parentDiv
+    if (findExit()) {
+      parentDiv = document.getElementsByClassName('gpt-translate')[0]
+    } else {
+      parentDiv = document.createElement('div')
+    }
+
+    let show = true
+    if (!document.getSelection().toString()) {
+      show = false
+    }
+
+    parentDiv.setAttribute('class', 'gpt-translate')
+    console.log(document.getSelection().toString())
+
+    parentDiv.setAttribute('style', `display: ${!show ? 'none' : 'block'}; position: absolute; left: ${right}px; top: ${offSet(focusNode).top + 30}px; width: 30px; height: 30px; background: black; color: white; text-align: center; line-height: 30px; border-radius: 3px; cursor: pointer;`)
+    const p = document.createElement('p')
+    p.innerText = '译'
+    parentDiv.appendChild(p)
+    body.appendChild(parentDiv)
+  } catch (error) {
+    findExit()
+    console.warn('plugins error', error)
   }
-  parentDiv.setAttribute('class', 'gpt-translate')
-  console.log(focusNode)
-  const pos = focusNode.getBoundingClientRect()
-  const right = Math.ceil(pos.right / window.devicePixelRatio)
-  const top = Math.ceil(offSet(focusNode).top) - 30
-  parentDiv.setAttribute('style', `position: absolute; left: ${right}px; top: ${top}px; width: 30px; height: 30px; background: black; color: white; text-align: center; line-height: 30px; border-radius: 3px; cursor: pointer;`)
-  const p = document.createElement('p')
-  p.innerText = '译'
-  parentDiv.appendChild(p)
-  body.appendChild(parentDiv)
 }
 
 document.addEventListener('selectionchange', debounce(appIncon, 1000))
+document.addEventListener('mouseup', function (event) {
+  console.log(event)
+  right = event.x
+})
